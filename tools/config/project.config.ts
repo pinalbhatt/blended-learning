691db16922f4ea862cf253f8709dd1a61adc3aed
Pinal Bhatt <Pinal.Bhatt@weightwatchers.com>
2016-03-25 10:24:33 -0400

    commit

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -20,7 +20,8 @@ export class ProjectConfig extends SeedConfig {
     this.APP_ASSETS = [
       // {src: `${this.ASSETS_SRC}/css/toastr.min.css`, inject: true},
       // {src: `${this.APP_DEST}/assets/scss/global.css`, inject: true},
-      { src: `${this.ASSETS_SRC}/main.css`, inject: true },
+      { src: `${this.ASSETS_SRC}/css/mdb.css`, inject: true },
+      { src: `${this.ASSETS_SRC}/css/style.css`, inject: true }
     ];
   }
 }
