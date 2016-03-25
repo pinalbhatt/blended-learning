267639f0a590e908c21aea3a3c44ed85578f0615
Pinal Bhatt <Pinal.Bhatt@weightwatchers.com>
2016-03-25 16:08:38 -0400

    demo title service communication between parent and child component

--- src/ngApp/demo/youtube-playlist/youtube-playlist.service.ts
+++ src/ngApp/demo/youtube-playlist/youtube-playlist.service.ts
@@ -1,13 +1,14 @@
 import {Injectable} from "angular2/core";
 import {Http, Response} from "angular2/http";
-import Observable from 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
+
 
 @Injectable()
 export class YoutubePlaylistService{
     constructor(private _http: Http){}
 
     getPlaylist(playlistId: string) {
-      
+
         var url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=AIzaSyCscnmFBOxRBO36Az1Ytlw0K1KAVieGa64&playlistId=" + playlistId;
         return this._http.get(url)
             .map(function (response: Response) {
