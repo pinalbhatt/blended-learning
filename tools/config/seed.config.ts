4c4990c52556a76c07d669e3b3a73b9f6f85ee51
Pinal Bhatt <Pinal.Bhatt@weightwatchers.com>
2016-03-29 21:24:11 -0400

    getting latest from angular2-seed

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -1,4 +1,3 @@
-import {readFileSync} from 'fs';
 import {argv} from 'yargs';
 import {normalize, join} from 'path';
 import {InjectableDependency, Environments} from './seed.config.interfaces';
@@ -48,7 +47,7 @@ export class SeedConfig {
   VERSION_NPM          = '2.14.2';
   VERSION_NODE         = '4.0.0';
 
-  NG2LINT_RULES        = customRules();
+  CODELYZER_RULES      = customRules();
 
   NPM_DEPENDENCIES: InjectableDependency[] = [
     { src: 'systemjs/dist/system-polyfills.src.js', inject: 'shims' },
@@ -186,12 +185,12 @@ export function normalizeDependencies(deps: InjectableDependency[]) {
 }
 
 function appVersion(): number|string {
-  var pkg = JSON.parse(readFileSync('package.json').toString());
+  var pkg = require('../../package.json');
   return pkg.version;
 }
 
 function customRules(): string[] {
-  var lintConf = JSON.parse(readFileSync('tslint.json').toString());
+  var lintConf = require('../../tslint.json');
   return lintConf.rulesDirectory;
 }
 
